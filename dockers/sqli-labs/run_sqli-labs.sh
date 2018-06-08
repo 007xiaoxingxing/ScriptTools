62ec0a3a426afaa8428297cb59aa3ff44ad9454a
star-chen <chanxing9@gmail.com>
2018-06-08 11:00:59 +0800

    remove some file

--- dockers/sqli-labs/run_sqli-labs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --privileged -p 82:80 -itd --name sqlilab sqli-labs
--- dockers/sqli-labs/sqli_lab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/libexec/mariadb-prepare-db-dir %n
-/usr/bin/mysqld_safe --basedir=/usr >/dev/null 2>&1 &
-/usr/sbin/httpd -DFOREGROUND
